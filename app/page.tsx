5345804ceed29f3cd3eb46136e9172b6170dc28d
Noor <norxxxxnor@gmail.com>
2026-08-26 01:02:52 +0300

    parallax section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+'use client'
+import { motion, useScroll, useTransform } from "motion/react";
 import Image from "next/image";
+import { useRef } from "react";
 
 export default function Home() {
   return (
@@ -160,9 +163,42 @@ export default function Home() {
         </div>
       </section>
 
-      <section>
-        
+      <section className="h-screen w-screen">
+        <Section />
       </section>
+      
+      <section  className="h-screen bg-white"></section>
     </>
   );
+}
+
+
+function Section() {
+  const container = useRef<HTMLDivElement>(null);
+   const { scrollYProgress } = useScroll({
+      target: container,
+
+      offset: ["start end", 'end start']
+    })
+
+    const y = useTransform(scrollYProgress, [0, 1], ["-10vh", "10vh"]);
+   return (
+
+      <div
+      ref={container} 
+      className='relative flex items-center justify-center h-screen overflow-hidden'
+      style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}
+      >
+        <div className='fixed top-[-10vh] left-0 h-[120vh] w-full'>
+          <motion.div style={{ y }} className='relative w-full h-full'>
+
+            <Image src={"/archeticture.jpeg"} fill alt="image" style={{objectFit: "cover"}}/>
+
+          </motion.div>
+        </div>
+
+      </div>
+
+    )
+  
 }
